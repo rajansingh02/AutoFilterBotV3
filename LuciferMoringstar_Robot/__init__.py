d0ac5bb13e5c8b3136b67b3fe47a4e0d449eea2a
Rajan Singh <84074133+rajansingh02@users.noreply.github.com>
2022-07-27 02:11:38 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -29,13 +29,8 @@ Bot Owner Only
 
 ABOUT = """
 ➥ Name :  Auto Filter V3
-    
-➥ Creator : [Naughty PROFESSOR](
-https://youtube.com/c/NaughtyPROFESSOR)
 
 ➥ Language : Python3
 
 ➥ Library : Pyrogram Asyncio 1.13.0
-
-➥ Tutorial : [Click Me](https://youtu.be/5hnYOKBzyi8)
 """
